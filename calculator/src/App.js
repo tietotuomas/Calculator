a0c3e6dcb9b15005659d6d5bb9b433732f049a4b
tietotuomas <tietotuomas@gmail.com>
2023-04-15 22:59:42 +0300

    Add initial functionality

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [result, setResult] = useState('')
+  const [number, setNumber] = useState(0)
+  const [calculation, setCalculation] = useState('')
+
+  const add = () => {
+    if (result === '') {
+      setResult(parseInt(number))
+    } else {
+      setResult(parseInt(result) + parseInt(number))
+    }
+
+    setCalculation(calculation + ' + ' + number)
+    setNumber('')
+  }
+
+  const subtract = () => {
+    if (result === '') {
+      setResult(parseInt(number) * -1)
+      setCalculation('-' + calculation + ' - ' + number)
+    } else {
+      setResult(parseInt(result) - parseInt(number))
+      setCalculation(calculation + ' - ' + number)
+    }
+
+    setNumber('')
+  }
+
+  const reset = () => {
+    setResult('')
+    setNumber(0)
+    setCalculation('')
+  }
+
+  const handleNumberChange = (event) => {
+    setNumber(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Calculator</h2>
+
+      <button onClick={add}>+</button>
+      <button onClick={subtract}>-</button>
+      <button onClick={reset}>Reset</button>
+      <br></br>
+      <input type="number" value={number} onChange={handleNumberChange} />
+      <br></br>
+
+      {result === '' ? null : (
+        <>
+          <p>{calculation.substring(2)} =</p>
+          <p>
+            <b>{result}</b>
+          </p>
+        </>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
